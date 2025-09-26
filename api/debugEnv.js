23980e28c6cc3640504d21a6314e9d6973910a59
Alexis <aguirrealexis.cba@gmail.com>
2025-09-26 18:21:38 -0300

    debug: expandir debugEnv para mostrar todas las variables Firebase

--- api/debugEnv.js
+++ api/debugEnv.js
@@ -1,13 +1,27 @@
 // api/debugEnv.js
 export default function handler(req, res) {
-  res.json({
-    tokenSet: !!process.env.META_WA_TOKEN,
-    phoneIdSet: !!process.env.META_WA_PHONE_ID,
-    verifySet: !!process.env.META_WA_VERIFY_TOKEN,
+  const envStatus = {
+    // WhatsApp variables
+    tokenSet: !!process.env.WHATSAPP_TOKEN,
+    phoneIdSet: !!process.env.PHONE_NUMBER_ID,
+    verifySet: !!process.env.VERIFY_TOKEN,
+    
+    // Firebase variables (nuevas)
     firebaseProjectId: !!process.env.FIREBASE_PROJECT_ID,
     firebaseClientEmail: !!process.env.FIREBASE_CLIENT_EMAIL,
     firebasePrivateKey: !!process.env.FIREBASE_PRIVATE_KEY,
     firebaseStorageBucket: !!process.env.FIREBASE_STORAGE_BUCKET,
     firebaseServiceAccount: !!process.env.FIREBASE_SERVICE_ACCOUNT,
-  });
+    
+    // Firebase variables (viejas - para verificar)
+    fbProjectId: !!process.env.FB_PROJECT_ID,
+    fbClientEmail: !!process.env.FB_CLIENT_EMAIL,
+    fbPrivateKey: !!process.env.FB_PRIVATE_KEY,
+    
+    // Valores reales (solo primeros caracteres para debug)
+    firebaseProjectIdValue: process.env.FIREBASE_PROJECT_ID?.substring(0, 10) + '...',
+    fbProjectIdValue: process.env.FB_PROJECT_ID?.substring(0, 10) + '...',
+  };
+
+  res.status(200).json(envStatus);
 }
