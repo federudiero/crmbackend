63e64f99032cdee2f20f8841b14eae3f901b1e05
Alexis <aguirrealexis.cba@gmail.com>
2025-09-26 17:18:28 -0300

    fix: unificar variables de entorno Firebase para solucionar error de bucket

--- api/debugEnv.js
+++ api/debugEnv.js
@@ -4,5 +4,10 @@ export default function handler(req, res) {
     tokenSet: !!process.env.META_WA_TOKEN,
     phoneIdSet: !!process.env.META_WA_PHONE_ID,
     verifySet: !!process.env.META_WA_VERIFY_TOKEN,
+    firebaseProjectId: !!process.env.FIREBASE_PROJECT_ID,
+    firebaseClientEmail: !!process.env.FIREBASE_CLIENT_EMAIL,
+    firebasePrivateKey: !!process.env.FIREBASE_PRIVATE_KEY,
+    firebaseStorageBucket: !!process.env.FIREBASE_STORAGE_BUCKET,
+    firebaseServiceAccount: !!process.env.FIREBASE_SERVICE_ACCOUNT,
   });
 }
